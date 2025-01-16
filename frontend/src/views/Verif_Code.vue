acc59ac7a43a953cc5608d2c7d62c87ad233965d
Fanny Carré <fannycarre650@gmail.com>
2025-01-16 09:51:54 +0100

    le script en commentaires parce que ça bug

--- frontend/src/views/Verif_Code.vue
+++ frontend/src/views/Verif_Code.vue
@@ -4,12 +4,12 @@
             <img src="../assets/images/logo.png" alt="logo"/>
         </div>
         <h2>Entrez le code de confirmation</h2>
-        <form action="/verif-code" method="POST">
+        <form @submit.prevent="verif_code">
             <div class="form-group mb-2">
                 <label for="code">Code</label>
                 <div class="input-container">
-                    <input id="code" name="code" type="text" placeholder="Ex: 1234" required>
-                    <input type="hidden" name="email" value= "<%= email %>" >
+                    <input id="code" name="code" type="text" placeholder="Ex: 1234" v-model="form_data.text" required>
+                    <input type="hidden" name="email" value= "{{ email }}" v-model="form_data.email" >
                 </div>
             </div>
             <p class="error"> {{ errorMessage }}</p>
@@ -20,5 +20,28 @@
 
 
 <script setup>
+/** 
+    import { ref } from 'vue'
+    import router from '../router'
+
+    const form_data = ref({})
+    const errorMessage = ref("")
+
+    async function verif_code() {
+        const response = await fetch('/auth/verif_code', {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(form_data.value),
+        })
+
+        if (response.status === 200) {
+            router.push('visite')
+        } else if (response.status === 402) {
+            errorMessage.value = "Code expiré"
+        } else if (response.status === 403){
+            errorMessage.value = "Code invalide"
+        }
+    }
+*/
 
 </script>
